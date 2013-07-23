a4545fbd36d787552ce60499387d5776ab3464d8
Justice London <jlondon@syrussystems.com>
2013-07-23 11:44:20 -0400

    Final changes for a version 0.0.2 module.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,20 +1,28 @@
+#Installs configuration files for the trocla agent/CLI
+#
+#Options
+# [*adapter*] Defines the adapter type to use for trocla agent. Generally YAML
+# [*adapter_options*] This will contain a hash of the actual options to pass the
+# trocla configuration. Generally you might pass the file option for key-file
+# [*keysize*] Define the length of default passwords to create. 16 by default
 class trocla::config (
-  $adapter = undef,
-  $adapter_options = [ 'default' => '' ],
+  $adapter         = undef,
+  $keysize         = 16,
+  $adapter_options = { 'default' => '' },
 ) {
   require trocla::master
 
-# deploy default config file and link it for trocla cli lookup
+# Deploy default config file and link it for trocla cli lookup
   file{
-    #should be
     "${settings::confdir}/troclarc.yaml":
       ensure  => present,
       content => template('trocla/troclarc.yaml.erb'),
       owner   => root,
       group   => puppet,
-      mode    => 0640;
+      mode    => '0640';
     '/etc/troclarc.yaml':
       ensure => link,
       target => "${settings::confdir}/troclarc.yaml";
   }
+
 }
